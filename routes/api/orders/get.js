4aa9ff876c97c27f6685b13a808fb4a91cf5d911
xgro <zero1401@naver.com>
2022-06-13 18:11:29 +0900

    Feat: orders 업데이트

--- routes/api/orders/get.js
+++ routes/api/orders/get.js
@@ -1,11 +1,9 @@
 'use strict'
 
-const { ObjectId } = require("@fastify/mongodb")
-
 module.exports = async function (fastify, opts) {
     fastify.get('/', async function (request, reply) {
-        const collection = this.mongo.db.collection('restaurants')
-        const result = await collection.findOne({ _id: ObjectId("62a5776ea1a55a9af7bdbde0") })
+        const collection = this.mongo.db.collection('order')
+        const result = await collection.find({}).toArray()
 
         reply
             .code(200)
